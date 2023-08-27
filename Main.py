211a5167e563e4c0fa1d90c35787349a44af846e
naomicherono <nonniecheronoh12@gmail.com>
2023-08-27 22:46:55 +0300

    Adding sample test for the code

--- Main.py
+++ Main.py
@@ -17,6 +17,15 @@ if __name__ == "__main__":
     review2 = Review(customer2, restaurant1, 5)
     review3 = Review(customer1, restaurant2, 3)
 
+     # Testing methods
+    print(customer1.full_name())  # Output: Naomi Cherono
+    print(restaurant1.get_name())  # Output: Restaurant A
+    print(review1.get_rating())    # Output: 4
+    print(customer1.restaurants()) # Output: ['Restaurant A', 'Restaurant B']
+    print(restaurant1.customers()) # Output: ['Naomi Cherono', 'Leonard Mutai']
+    print(restaurant1.average_star_rating())  # Output: 4.5
+
+
 
 
 
