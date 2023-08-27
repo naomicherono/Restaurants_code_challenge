221fc20020ea1235516540bb05522734e2ac392e
naomicherono <nonniecheronoh12@gmail.com>
2023-08-27 22:43:54 +0300

    Added example customer profiles: Naomi Cherono and Leonard Mutai

--- Main.py
+++ Main.py
@@ -3,4 +3,8 @@ from Restaurant import Restaurant
 from Review import Review
 
 if __name__ == "__main__":
+     # Creating customers names
+    customer1 = Customer("Naomi", "Cherono")
+    customer2 = Customer("Leonard", "Mutai")
+
 
