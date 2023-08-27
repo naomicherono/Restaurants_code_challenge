1988048c401cef50612e10c16e1867491634e449
naomicherono <nonniecheronoh12@gmail.com>
2023-08-27 22:35:00 +0300

    Implementing Customer class constructor and methods

--- Customer.py
+++ Customer.py
@@ -1,2 +1,26 @@
 class Customer:
     all_customers = []
+
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        self.reviews = []
+        Customer.all_customers.append(self)
+        
+    def get_given_name(self):
+        return self.given_name
+    
+    def get_family_name(self):
+        return self.family_name
+    
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+    
+    def restaurants(self):
+        return [review.restaurant.get_name() for review in self.reviews]
+    
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+
